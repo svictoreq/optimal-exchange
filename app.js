f2c13b7e4dc5e157b9819c730b813112d3141e1e
Victor Requena <svictoreq@gmail.com>
2018-12-04 00:07:02 -0400

    Add input reading and manipulation capabilities

---
+++ app.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+
+const input = fs.readFileSync('./input.txt', 'utf8');
+const lines = input.split('\n');
+const result = lines.reduce((obj, line, index) => {
+	const val = `line${index}`;
+	if (line.split(' ').length < 3) return { ...obj, testsQty: Number(line) };
+	const test = {
+		n: Number(line.split(' ')[0]),
+		k: Number(line.split(' ')[1]),
+		d: line.split(' ')
+			.slice(2)
+			.map((num) => Number(num)),
+	};
+	return {
+		...obj,
+		tests: [ ...obj.tests, test ],
+	};
+}, { tests: 0, tests: [] });
